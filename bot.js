5ce1b5396162e447e0caee7e89f95d5d4458af67
NickelOw1 <nermakov142@gmail.com>
2022-05-15 03:50:10 +0700

    added ascii visualization to daily puzzle, added first realization of "move" command, deleted useless commands

--- bot.js
+++ bot.js
@@ -1,11 +1,10 @@
-// Require the necessary discord.js classes
 const { Client, Intents, MessageAttachment } = require('discord.js');
 require('dotenv').config()
 const axios = require('axios')
-// Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 const Canvas = require('canvas');
-// When the client is ready, run this code (only once)
+const {Chess} = require("fix-esm").require("chess.js");
+
 client.once('ready', () => {
 	console.log('Ready!');
 });
@@ -17,25 +16,39 @@ client.on('interactionCreate', async interaction => {
 
 	if (commandName === 'ping') {
 		await interaction.reply('Pong!');
-	} else if (commandName === 'server') {
-		await interaction.reply('Server info.');
-	} else if (commandName === 'user') {
-		await interaction.reply('User info.');
 	} else if (commandName === 'daily') {
 		const axiosResponse = await axios.get('https://lichess.org/api/puzzle/daily')
-		interaction.reply(axiosResponse.data.game.pgn)
+		const chess = Chess()
+		movesArray = axiosResponse.data.game.pgn.split(" ");
+		movesArray.forEach((item) => {
+			chess.move(item)
+		})
+		interaction.reply("```"+chess.ascii()+"```")
 	} else if (commandName === 'play') {
+		const chess = Chess()
+		console.log(chess.pgn())
+		while (!chess.game_over()) {
+			const moves = chess.moves()
+			const move = moves[Math.floor(Math.random() * moves.length)]
+			chess.move(move)
+		}
+		console.log(chess.pgn())
+		return
 		const canvas = Canvas.createCanvas(700, 700);
 		const context = canvas.getContext('2d');
 		const background = await Canvas.loadImage('./board.jpeg');
 
-		// This uses the canvas dimensions to stretch the image onto the entire canvas
 		context.drawImage(background, 0, 0, canvas.width, canvas.height);
 	
-		// Use the helpful Attachment class structure to process the file for you
 		const attachment = new MessageAttachment(canvas.toBuffer(), 'profile-image.png');
 	
 		interaction.reply({ files: [attachment] });
+	} else if (commandName === 'move') {
+		const playerMove = interaction.options.getString('move');
+		interaction.reply(playerMove)
+	} else if (commandName === 'say') {
+		const sayMessage = interaction.options.getString('input');
+		interaction.reply(sayMessage)
 	}
 });
 
--- deploy-commands.js
+++ deploy-commands.js
@@ -7,9 +7,9 @@ require('dotenv').config()
 const commands = [
 	new SlashCommandBuilder().setName('daily').setDescription('Gets the daily puzzle from lichess'),
 	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
 	new SlashCommandBuilder().setName('play').setDescription('Starts the game'),
+	new SlashCommandBuilder().setName('move').setDescription('Receives your move').addStringOption(option => option.setName('move').setDescription('your move').setRequired(true)),
+	new SlashCommandBuilder().setName('say').setDescription('Says you told them to say').addStringOption(option => option.setName('input').setDescription('The input to echo back').setRequired(true)),
 ]
 	.map(command => command.toJSON());
 
